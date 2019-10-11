e23236a76335710bd44243ad665c41ad58503e2d
Jae Yong Ryu <jyryu@krict.re.kr>
2019-10-11 14:08:14 +0900

    Adde install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+conda install -y -c rdkit -c mordred-descriptor mordred
+conda install -y -c bioconda pybel
+pip install numpy==1.16.4
+pip install tensorflow==1.13.1
+pip install pandas==0.24.2
+pip install scikit-learn==0.20.2
+pip install scipy==1.2.1
+pip install git+https://github.com/gadsbyfly/PyBioMed
