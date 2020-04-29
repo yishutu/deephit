0ad54b4baface14952024aaf5b767eef6830d59f
Jae Yong Ryu <jyryu3161@kaist.ac.kr>
2020-04-29 08:30:28 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 conda install -y -c rdkit -c mordred-descriptor mordred
 conda install -y -c conda-forge pybel
+conda install -y -c openbabel openbabel=3.0.0
 pip install numpy==1.16.4
 pip install tensorflow==1.13.1
 pip install pandas==0.24.2
